57b9ccc79ef21a807cd4a007daa19c41421b4990
Abhishek-725 <101037940+Abhishek-725@users.noreply.github.com>
2022-03-06 17:54:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+//document.getElementById('#menu')
+
+
+function open() {
+/*  document.getElementById("main").style.marginLeft = "25%"; */
+  document.getElementById("sidebar").style.width = "320px";
+  document.getElementById("sidebar").style.display = "block";
+  
+  
+}
